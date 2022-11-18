a6aaa394044efc438d4d9a39c852dcdc787c6db4
Summer <56782157+Thangtran1100@users.noreply.github.com>
2022-11-18 11:54:42 -0800

    Just change the image capture to save in the folder image-capture

--- 11_18_2022/imageCapture.py
+++ 11_18_2022/imageCapture.py
@@ -1,5 +1,7 @@
 from djitellopy import Tello
+import KeyPressModule as key
 import cv2
+import time
 
 width = 320
 height = 240
@@ -9,13 +11,15 @@ drone = tello.Tello()
 drone.connect()
 print(drone.get_battery())
 
+key.init()
 drone.streamon()
 
 # 0: forward camera
 # 1: bottom camera
 drone.set_video_direction(1)
 while True:
-    img = drone.get_frame_read().frame
+    image = drone.get_frame_read().frame
     #img = cv2.resize(img, (360,240))
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", image)
+    cv2.imwrite(f'11_18_2022/image-capture/{time,time()}.jpg', image)
     cv2.waitKey(1)
