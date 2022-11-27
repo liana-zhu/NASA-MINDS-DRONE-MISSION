f8d0ce3093d36cabb8da388e63457fe9a3ea2d23
Summer <56782157+Thangtran1100@users.noreply.github.com>
2022-11-27 12:08:23 -0800

    updated files for connect drone and show pictures

--- 11_18_2022/imageCapture.py
+++ 11_18_2022/imageCapture.py
@@ -1,7 +1,10 @@
+import cv2
 from djitellopy import Tello
 import keyboard as key
-import cv2
+import os
 import time
+
+global image
 
 width = 320
 height = 240
@@ -11,28 +14,39 @@ drone = Tello()
 drone.connect()
 print(drone.get_battery())
 
-key.init()
+
+dir_path = 'D:\\NASA-MINDS-DRONE-MISSION\\11_18_2022\\image-capture'
+#os.makedirs(dir_path, exist_ok=True)
+#base_path = os.path.join(dir_path, base)
+n = 0
+
 drone.streamon()
 
 # 0: forward camera
 # 1: bottom camera
-drone.set_video_direction(1)
+#drone.set_video_direction(0)
 
-drone.takeoff()
+#drone.takeoff()
 
 while True:
     image = drone.get_frame_read().frame
     #img = cv2.resize(img, (360,240))
-    cv2.imshow("Image", image)
-    cv2.imwrite(f'11_18_2022/image-capture/{time,time()}.jpg', image)
-    drone.rotate_clockwise(90)
-    time.sleep(3)
-    drone.move_left(35)
-    time.sleep(3)
+    
+    cv2.imshow('Figure 1', image)
+    cv2.waitKey(0)
+    cv2.show()
+    
+    # drone.rotate_clockwise(90)
+    # time.sleep(3)
+    # drone.move_left(35)
+    # time.sleep(3)
     # if cv2.waitKey(32) == ord('Space'):
     #     drone.land()
     #     break
     
-    if key.read_key() == 'space':
-        drone.land()
-        break
+    # if key.read_key() == 'space':
+    #     drone.land()
+    #     break
+    cv2.imwrite(f'D:\\NASA-MINDS-DRONE-MISSION\\11_18_2022\\image-capture\\{time.time()}.jpg', image)
+    
+cv2.destroyAllWindows()
